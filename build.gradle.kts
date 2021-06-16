ca6a94badd14582ee42b432463de6818b3481cce
Tyler Cook <tcc@sandpolis.com>
2021-06-15 20:34:23 -0500

    Add publishing workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
 plugins {
     id("java-library")
     id("eclipse")
+    id("maven-publish")
+    id("signing")
+
+    id("org.ajoberstar.grgit") version "4.1.0"
+}
+
+import org.ajoberstar.grgit.Grgit
+
+// Set project version according to the latest git tag
+val gitDescribe = Grgit.open {
+    currentDir = project.getProjectDir().toString()
+}.describe { tags = true }
+
+if (gitDescribe == null) {
+    project.version = "0.0.0"
+} else {
+    project.version = gitDescribe.replaceFirst("^v".toRegex(), "")
 }
 
 repositories {
@@ -42,4 +59,54 @@ eclipse {
 		name = project.name
 		comment = project.name
 	}
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.github.cilki"
+            artifactId = "qcow4j"
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Qcow2 driver written in Java")
+                url.set("https://github.com/cilki/${project.name}")
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("cilki")
+                        name.set("Tyler Cook")
+                        email.set("tcc@sandpolis.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/cilki/${project.name}.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/cilki/${project.name}.git")
+                    url.set("https://github.com/cilki/${project.name}")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/cilki/${project.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+signing {
+    useInMemoryPgpKeys(System.getenv("SIGNING_PGP_KEY") ?: "", System.getenv("SIGNING_PGP_PASSWORD") ?: "")
+    sign(publishing.publications["mavenJava"])
 }
